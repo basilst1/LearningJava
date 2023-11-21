75c74647f0a149fa5b51c7b553fec11fc1def596
basilst1 <hellobasilst@gmail.com>
2023-11-20 20:49:17 -0700

    second commit

---
+++ src/WaterTankCapacity/tankCapMain.java
@@ -0,0 +1,13 @@
+package WaterTankCapacity;
+
+public class tankCapMain extends WaterCapacity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WaterCapacity ob = new WaterCapacity();
+		ob.fillTank(0);
+
+	}
+
+}
